e18995d45117768dd4e85044bb72d87154b43f97
Serge S. Gulin <gulin.serge@gmail.com>
2026-03-07 13:05:06 +0300

    [ lint ] Extract `super-linter` rules into a separate file, add `pack build super-linter-run`

---
+++ .linters/super-linter.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# See: https://github.com/super-linter/super-linter/blob/dd82ec5c927bff0fecc98865b0be8aa6ef849001/docs/run-linter-locally.md
+docker run --platform linux/amd64 \
+  --env-file "$(pwd)/../.github/linters/super-linter.env" -e LOG_LEVEL=NOTICE \
+  -e RUN_LOCAL=true \
+  -v "$(pwd)/../":/tmp/lint \
+  ghcr.io/super-linter/super-linter:latest
